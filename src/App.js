be89673dc40fe69da64cfca47d53fba1273ce6c4
Aniket <aniket.d2402@gmail.com>
2024-05-02 21:52:17 -0400

    added code for fetching data from a dummy api endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useEffect } from 'react';
 import Header from './Header';
 import Main from './Main';
+import { useReducer } from 'react';
+
+const initialState = {
+  questions: [],
+  status: 'loading', //status can be: loading, error, ready, active, finished
+};
+
+const reducer = (currentState, action) => {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...currentState, status: 'ready', questions: action.payload };
+    case 'dataFailed':
+      return { ...currentState, status: 'error' };
+    default:
+      throw new Error('Unknown Action');
+  }
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((error) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
     <div className='app'>
       <Header />
